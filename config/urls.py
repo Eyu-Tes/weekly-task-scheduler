d7ca2adf199e55a2e095496f8c7552c1cbcccaf0
Eyu-Tes <eyoabtesfaye10@gmail.com>
2020-09-08 13:53:48 +0300

    removed db.sqlite3 from .gitignore

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('scheduler.urls')),
 ]
-
-# urlpatterns += [
-#     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-# ]
